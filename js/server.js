d17d5e49e34047a1c5dd0a8992376249a2da9d2a
Mattdre <olsen_matthew@yahoo.com>
2016-07-06 15:23:15 -1000

    full communication between server and all clients, working on usernames

--- js/server.js
+++ js/server.js
@@ -7,11 +7,19 @@ const server = net.createServer((socket)=>{
     users.push(socket);
     console.log('client connected');
     socket.on('data', ( data ) => {
-      process.stdout.write(data.toString());
+      process.stdout.write('random user: ' + data.toString());
       for (var i = 0; i < users.length; i++) {
-        users[i].write(data);
+        if(users[i] !== socket){ //if sender is same as receiver dont send!
+          users[i].write('random user: ' + data);
+        }
       }
     });
+});
+
+process.stdin.on('data', (data) => {
+  for (var i = 0; i < users.length; i++) {
+    users[i].write('[ADMIN]: ' + data);
+  }
 });
 
 server.listen('6969', () =>{
